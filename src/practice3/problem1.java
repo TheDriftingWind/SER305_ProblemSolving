1d613ccff52ad320c556a36e276562e54f07daaf
Charles Zhu <CharlesYZhu@yahoo.com>
2017-09-16 06:01:44 -0400

    problem1 v1

--- src/practice3/problem1.java
+++ src/practice3/problem1.java
@@ -7,10 +7,43 @@ public class problem1 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
+		int partition = scan.nextInt();
+		
+		scan.nextLine(); //end the line
+		
 		String[] input = scan.nextLine().split(" ");
+
+		//initialize pointer starting indexes
+		int leftPointer = 0;
+		int rightPointer = input.length-1;
+		
+		//while left and right pointers have not past each other...
+		while(leftPointer < rightPointer){
+			if( Integer.parseInt(input[rightPointer]) <= partition && Integer.parseInt(input[leftPointer]) > partition ){
+				String c = input[rightPointer];
+				input[rightPointer] = input[leftPointer];
+				input[leftPointer] = c;
+				rightPointer--;
+				//swap(input[rightPointer], input[leftPointer]);	
+			}
+			leftPointer++;
+			
+		}
+		
+		for(int i = 0; i < input.length; i++){
+			
+			System.out.print(input[i] + " ");
+			
+		}
 		
 		scan.close();
 
 	}
+	
+//	public static void swap(String a, String b){
+//		String c = a;
+//		a = b;
+//		b = c;
+//	}
 
 }
