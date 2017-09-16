258fb7d61712fdb18b6d036d467a269be37a5876
Charles Zhu <CharlesYZhu@yahoo.com>
2017-09-16 17:26:57 -0400

    problem3 v1

---
+++ src/practice3/ShiftingArrayElements.java
@@ -0,0 +1,35 @@
+package practice3;
+
+import java.util.Scanner;
+
+public class ShiftingArrayElements {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		String[] input = scan.nextLine().split(" ");
+		int shift = scan.nextInt();
+		int[] storage = new int[input.length];
+		
+		//run through element of the array of inputs
+		for(int i = 0; i < input.length; i++){
+			//the index to insert the current element in the input is at (currentIndex + length of the array - shift) modulus 5
+			int insertAt = (i + input.length - shift) % 5;
+			//insert the element
+			storage[insertAt] = Integer.parseInt(input[i]);
+		}
+		
+		//print out the results
+		for(int i = 0; i < storage.length; i++){
+			System.out.print(storage[i] + " ");
+		}
+		
+		
+		
+		//
+		
+		
+		scan.close();
+	}
+
+}
