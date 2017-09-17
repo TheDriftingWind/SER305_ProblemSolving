5b8208029167209ef738f0d5e9da78106d7c47ab
Charles Zhu <CharlesYZhu@yahoo.com>
2017-09-16 21:53:39 -0400

    Problem4 v1

--- src/practice3/ArrangingPebbles.java
+++ src/practice3/ArrangingPebbles.java
@@ -1,5 +1,6 @@
 package practice3;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrangingPebbles {
@@ -7,18 +8,35 @@ public class ArrangingPebbles {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
-		String input = scan.nextLine().replaceAll("\\s","");
-		System.out.println(input);
+		String[] input = scan.nextLine().split(" ");
+		
 		String map = "wgr";
 		int letter = 0;
-		int pointerX = 0;
-		int pointerY = 1;
-		//start at the beginning of the array
-		//if the character is == the letter we want, advance pointer X by one...
-		//else look for the letter we need by advancing k until we find it and swap
-		//when k reaches the end of the array, change the 
+		//int pointer = 1;
 		
-		// use this method on a string form of the input map.indexOf(ch, fromIndex)
+		//start at the first element
+		for(int i = 0; i < input.length; i++){
+			//if the current element doesn't match the letter we want...
+			if (input[i] != String.valueOf(map.charAt(letter))){
+				//find where the index where the letter occurs last...
+				int replace = Arrays.asList(input).lastIndexOf( String.valueOf(map.charAt(letter)) );
+				
+				//if it occurs at or before the index, there's no more of the letter that needs to be sorted...
+				if(replace <= i){
+					//move on to the next letter.
+					letter++;
+				}else{
+					//if there are more occurrences, find it and swap the current element with the needed letter
+					input[replace] = input[i];
+					input[i] = String.valueOf(map.charAt(letter));
+				}
+				
+			}
+		}
+		
+		for(int i = 0; i < input.length; i++){
+			System.out.print(input[i] + " ");
+		}
 		
 		scan.close();
 
