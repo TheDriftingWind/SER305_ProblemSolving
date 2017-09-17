6bcbce1249dd8f2fd40c880f9ee4a1ff35c0dbee
Charles Zhu <CharlesYZhu@yahoo.com>
2017-09-17 16:56:12 -0400

    Problem1 fix

--- src/practice3/ArrayPartitioning.java
+++ src/practice3/ArrayPartitioning.java
@@ -19,16 +19,26 @@ public class ArrayPartitioning {
 		
 		//while left and right pointers have not past each other...
 		while(leftPointer < rightPointer){
+			//if the rightPointer is less than the partition and the leftPointer is greater than the partition...
 			if( Integer.parseInt(input[rightPointer]) <= partition && Integer.parseInt(input[leftPointer]) > partition ){
+				//swap them
 				String c = input[rightPointer];
 				input[rightPointer] = input[leftPointer];
 				input[leftPointer] = c;
-				rightPointer--;
-				//swap(input[rightPointer], input[leftPointer]);	
+				//move the rightPointer back if you swap
+				
 			}
-			leftPointer++;
-			
+			//If the leftPointer is a number that is smaller or equal to the partition, it's in the right place - move on
+			if(Integer.parseInt(input[leftPointer]) <= partition){
+				leftPointer++;
+			}
+			//If the rightPointer finds a number that is large than the partition, it's in the right place - move on
+			if(Integer.parseInt(input[rightPointer]) > partition){
+				rightPointer--;
+			}
+				
 		}
+		//System.out.println("Partition: " + partition);
 		
 		for(int i = 0; i < input.length; i++){
 			
