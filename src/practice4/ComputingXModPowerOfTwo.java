4c531fc2a80a164f7d42d669db553b0247875be9
Charles Zhu <CharlesYZhu@yahoo.com>
2017-09-27 01:16:54 -0400

    Commented part 2

--- src/practice4/ComputingXModPowerOfTwo.java
+++ src/practice4/ComputingXModPowerOfTwo.java
@@ -10,9 +10,6 @@ public class ComputingXModPowerOfTwo {
 		int input = scan.nextInt();
 		int mod = scan.nextInt();
 		
-		System.out.println(Integer.toBinaryString(input));
-		System.out.println(Integer.toBinaryString(mod-1));
-		
 		/* Take the mod and subtract 1 to convert right most 1 to a 0 and all the 0's to 1's.
 		 * bitwise AND (mod-1) to the input number.
 		 * this reduces the input number to the mod number and only keeps the remainder(where 1 --> 1) 
