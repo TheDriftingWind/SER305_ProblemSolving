dca7f6c0e40d9ba1c2fb4cd2f920fe4930b52a65
Charles Zhu <CharlesYZhu@yahoo.com>
2017-10-18 00:15:33 -0400

    Problem1c v2

--- src/practice6/FindingCommonElementsInTwoSortedLists.java
+++ src/practice6/FindingCommonElementsInTwoSortedLists.java
@@ -16,37 +16,43 @@ public class FindingCommonElementsInTwoSortedLists {
 		ArrayList<Integer> matches = new ArrayList<Integer>();
 		
 		//start with the first character in list m
-		while(pointer_i < list_m.length-1){
+		while(pointer_i <= list_m.length-1 && pointer_c <= list_n.length-1){
 			//check against characters from list n until you find a matching number or a larger number
 			
 			//if you find a matching number, add it to the list of matches
 			if(Integer.parseInt(list_m[pointer_i]) == Integer.parseInt(list_n[pointer_c])){
 				matches.add(Integer.parseInt(list_m[pointer_i]));
-				while(Integer.parseInt(list_m[pointer_i]) == last_num && pointer_i < list_m.length-1){
+				while(pointer_i < list_m.length && Integer.parseInt(list_m[pointer_i]) == last_num){
 					pointer_i++; //find the next new number
 				}
-				last_num = Integer.parseInt(list_m[pointer_i]);
+				if(pointer_i < list_m.length){
+					last_num = Integer.parseInt(list_m[pointer_i]);
+				}
 			}
 			//if you find a larger number, move to the next item in m
 			else if(Integer.parseInt(list_m[pointer_i]) < Integer.parseInt(list_n[pointer_c])){
-				while(Integer.parseInt(list_m[pointer_i]) == last_num && pointer_i < list_m.length-1){
+				while(pointer_i < list_m.length && Integer.parseInt(list_m[pointer_i]) == last_num){
 					pointer_i++; //find the next new number
 				}
-				last_num = Integer.parseInt(list_m[pointer_i]);
+				if(pointer_i < list_m.length){
+					last_num = Integer.parseInt(list_m[pointer_i]);
+				}
 			}else{
 			//else keep checking the next item in n
 				pointer_c++;
 			}
 		}
-		
 		//You never move the pointers back, or double check. You only iterate through each list once
-		for(int j = 0; j < matches.size(); j++){
-			System.out.print(matches.get(j));
-			if(j != matches.size()-1){
-				System.out.print(" ");
+		if(matches.size() != 0){
+			for(int j = 0; j < matches.size(); j++){
+				System.out.print(matches.get(j));
+				if(j != matches.size()-1){
+					System.out.print(" ");
+				}
 			}
+		} else {
+			System.out.println("NO COMMON INTEGERS");
 		}
-
 	}
 
 }
