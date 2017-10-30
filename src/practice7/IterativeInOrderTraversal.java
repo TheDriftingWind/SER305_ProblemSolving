48420cf0e9b05dadab6eec6a8d7e842464727e03
Charles Zhu <CharlesYZhu@yahoo.com>
2017-10-29 22:10:30 -0400

    problem2 v1.c

--- src/practice7/IterativeInOrderTraversal.java
+++ src/practice7/IterativeInOrderTraversal.java
@@ -35,3 +35,38 @@ public class IterativeInOrderTraversal {
 		}
 	}
 }
+/*
+ * Time complexity O(n) where n = the number of nodes
+ * The algorithm is in O(n) time complexity because the number of actions it performs depends on
+ * how many nodes there are.
+ * The algorithm iterates through each node only twice, 
+ * once when it is added to the stack and once when it is removed from the stack.
+ * For each node, it performs a constant action that prints to data of the node.
+ */
+
+/*
+ * Space complexity O(h) where h = height of the tree
+ * The algorithm is in O(h) space complexity because at any given time,
+ * the maximum number of nodes that is stored in the node stack is equal to
+ * the height of the tree. The algorithm stops adding to the stack whenever it hits
+ * a leaf/node with no child, and never has any additional nodes than that as it proceeds to remove them.
+ */
+
+/*
+ * Pseudo code:
+ * 1.) Create an empty stack NodeStack
+ * 2.) Set a node pointer to the root
+ * 3.) While the pointer has a left child...
+ * 		push the node at the pointer onto the NodeStack
+ * 		move the pointer to the left child
+ * 4.)Push the node at the pointer onto the NodeStack
+ * (all nodes leading to the left-most leaf from the root should be on the stack)
+ * 5.) Until the stack is empty...
+ * 		move the pointer to the node on top of the stack, then pop the node from the stack
+ * 		print the value of that node
+ * 		If node at the pointer has a right child...
+ * 			move the pointer to the right child
+ * 			perform step 3
+ * 6.) Once the stack is empty, you're done
+ */
+
