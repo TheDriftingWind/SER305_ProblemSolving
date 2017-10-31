b2195874c20a0a9e3a899b532f3ccbb533f97c0a
Charles Zhu <CharlesYZhu@yahoo.com>
2017-10-30 23:07:16 -0400

    Problem1 v3.1c

--- src/practice7/DeterminingVisualAppealOfABinaryTree.java
+++ src/practice7/DeterminingVisualAppealOfABinaryTree.java
@@ -1,6 +1,5 @@
 package practice7;
 
-import java.util.ArrayList;
 import java.util.Stack;
 
 public class DeterminingVisualAppealOfABinaryTree {
@@ -10,12 +9,7 @@ public class DeterminingVisualAppealOfABinaryTree {
 		BinaryTreeNode<Integer> pointer = root;
 		Stack<BinaryTreeNode<Integer>> nodeStack = new Stack<BinaryTreeNode<Integer>>();
 		int counter = 0;
-		ArrayList<Integer> leafHeights = new ArrayList<Integer>();
-		
-		//add special case. The algorithm won't check for a right side on the root if none exists
-		if(root.getRightChild() == null){
-			leafHeights.add(counter);
-		}
+		int min = 0;
 		//Move the pointer to the left most node
 		while(pointer != null){
 			nodeStack.push(pointer);
@@ -23,8 +17,13 @@ public class DeterminingVisualAppealOfABinaryTree {
 			counter = counter + 1;
 		}
 		counter -= 1;	//decrement to account for the while loop counting the starting node as 1
-		leafHeights.add(counter);
+		int max = counter;
+		min = counter;
 		
+		//add special case - algorithm won't check for a right side if none exists
+		if(root.getRightChild() == null){
+			min = 0;
+		}
 		
 		//iterate through the stack
 		while(!nodeStack.isEmpty()){
@@ -37,7 +36,10 @@ public class DeterminingVisualAppealOfABinaryTree {
 				counter = counter + 1;
 				//if there is no right child, this is the rightmost leaf
 				if(pointer.getRightChild() == null){
-					leafHeights.add(counter);
+					if(counter > max)
+						max = counter;
+					if(counter < min)
+						min = counter;
 				}
 				//store the nodes in the stack as you pass them
 				while(pointer != null){
@@ -46,24 +48,15 @@ public class DeterminingVisualAppealOfABinaryTree {
 					counter = counter + 1;
 				}
 				counter -= 1; //decrement by one to get rid of the extra count of the first node
-				leafHeights.add(counter);
+				if(counter > max)
+					max = counter;
+				if(counter < min)
+					min = counter;
 			} else {
 				counter -= 1;
 			}
 		}
 		
-		//find the shortest branch and the longest branch
-		int max = 0;
-		int min = leafHeights.get(0);
-		for(int i = 0; i < leafHeights.size(); i++){
-			//System.out.println("index[" + i + "] - " + leafHeights.get(i));
-			if(leafHeights.get(i) > max)
-				max = leafHeights.get(i);
-			if(leafHeights.get(i) < min)
-				min = leafHeights.get(i);
-		}
-		
-		//System.out.println(max-min);
 		//if the difference between the shortest and longest branch is greater than 1, it's not balanced
 		if((max-min) > 1){
 			System.out.print("The binary tree IS NOT visually appealing.");
@@ -74,11 +67,18 @@ public class DeterminingVisualAppealOfABinaryTree {
 }
 
 /*
- * Time Complexity: O(n) where n is the number of nodes
+ * Time Complexity: O(n) where n is the number of nodes in the tree
+ * The algorithm is in O(n) time complexity because it only needs to run an inorder traversal,
+ * which does 1 pass through the tree of nodes. Thus, the number of actions the algorithm performs
+ * depends on the number of nodes in the tree.
  */
 
 /*
- * Space Complexity: O(h+l) where h is the height of the tree and l is the number of leaves
+ * Space Complexity: O(h) where h is the height of the tree and l is the number of leaves
+ * The space complexity is O(h) because the stack used to run the inorder traversal only holds a maximum
+ * number of nodes equal to the height of the tree. It never has more nodes than the height because it stops
+ * adding nodes when it reaches a leaf and removes nodes as it iterates.
+ * The other variables used in addition are a constant amount of integers used to store values.
  */
 
 /*
