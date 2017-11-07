44089124a437a49ea440606e88e239ab7831e967
TheDriftingWind <CharlesYZhu@yahoo.com>
2017-11-07 10:56:22 -0500

    problem2 v0.2

--- src/practice8/DeterminingIfBST.java
+++ src/practice8/DeterminingIfBST.java
@@ -1,6 +1,7 @@
 package practice8;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class DeterminingIfBST {
 
@@ -8,7 +9,33 @@ public class DeterminingIfBST {
 		Scanner scan = new Scanner(System.in);
 		int key = scan.nextInt();
 		BinaryTreeNode<Integer> root = BinaryTreeUtilities.readBinaryTreeFromText(scan);
-		
+		int max = root.getData();
+		int min = root.getData();
+		Stack<BinaryTreeNode<Integer>> stack = new Stack<BinaryTreeNode<Integer>>();
+		//preorder traversal
+		//keep track of min and max in a subtree
+		while(stack.empty()){
+			BinaryTreeNode<Integer> tempNode = stack.peek();
+			
+			//subtree
+			if(tempNode.getLeftChild().getData() < tempNode.getData()){
+				//NOT BST
+			}
+			if(tempNode.getRightChild().getData() > tempNode.getData()){
+				//NOT BST 
+			}
+			
+ 
+			//pop from stack
+			stack.pop();
+			//add left and right child
+			if(tempNode.getLeftChild() != null){
+				stack.push(tempNode.getLeftChild());
+			}
+			if(tempNode.getRightChild() != null){
+				stack.push(tempNode.getRightChild());
+			}
+		}
 	}
 
 }
