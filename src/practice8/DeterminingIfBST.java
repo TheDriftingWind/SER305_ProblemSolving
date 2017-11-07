11c63aa0f241ec6aa2a0e13b9b9e751560715fe5
Charles Zhu <CharlesYZhu@yahoo.com>
2017-11-06 19:54:24 -0500

    problem2 v0

---
+++ src/practice8/DeterminingIfBST.java
@@ -0,0 +1,14 @@
+package practice8;
+
+import java.util.Scanner;
+
+public class DeterminingIfBST {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int key = scan.nextInt();
+		BinaryTreeNode<Integer> root = BinaryTreeUtilities.readBinaryTreeFromText(scan);
+		
+	}
+
+}
